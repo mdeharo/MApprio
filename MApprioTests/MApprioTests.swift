50c92271051a32adf389d6e13acfbd2173a8e2d0
Mario de Haro Serón <mdeharo@viewnext.com>
2017-01-26 11:55:35 +0100

    Initial Commit

---
+++ MApprioTests/MApprioTests.swift
@@ -0,0 +1,36 @@
+//
+//  MApprioTests.swift
+//  MApprioTests
+//
+//  Created by Mario de Haro Serón on 26/1/17.
+//  Copyright © 2017 Bersusco. All rights reserved.
+//
+
+import XCTest
+@testable import MApprio
+
+class MApprioTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
