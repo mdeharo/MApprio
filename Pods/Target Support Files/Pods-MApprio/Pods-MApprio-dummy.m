b4fb1e8ec4167aef96c3c9199f63036c13bc60e5
Mario de Haro Serón <mdeharo@viewnext.com>
2017-01-26 12:31:25 +0100

    Added empty podfile  (init and install)

---
+++ Pods/Target Support Files/Pods-MApprio/Pods-MApprio-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MApprio : NSObject
+@end
+@implementation PodsDummy_Pods_MApprio
+@end
---
+++ Pods/Target Support Files/Pods-MApprioTests/Pods-MApprioTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MApprioTests : NSObject
+@end
+@implementation PodsDummy_Pods_MApprioTests
+@end
